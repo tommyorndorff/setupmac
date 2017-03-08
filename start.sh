e30b4652a58c9fd4f0b9b75c49ac830855355882
Tommy Orndorff <torndorff@expedia.com>
2017-03-08 07:15:46 -0500

    removing neovim

--- start.sh
+++ start.sh
@@ -38,7 +38,7 @@ if [ ! -d $installdir ]; then
     exit 1
 else
     cd $installdir 
-    ansible-playbook -i ./hosts playbook.yml --verbose
+    ansible-playbook -i ./hosts playbook.yml -vv
 fi
 
 echo "cleaning up..."
